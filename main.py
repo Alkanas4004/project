8b21c07ea145b13501e13667018149b965e7b537
Alkanas4004 <thestormleaksgang555@gmail.com>
2026-05-15 16:13:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import customtkinter as ctk
 from auth.login import LoginPage
 
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("blue")
+def main():
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
 
-app = ctk.CTk()
-app.geometry("1500x850")
-app.title("SuperMarket POS")
+    app = ctk.CTk()
+    app.geometry("1500x850")
+    app.title("SuperMarket POS")
 
-LoginPage(app)
+    LoginPage(app)
 
-app.mainloop()
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
